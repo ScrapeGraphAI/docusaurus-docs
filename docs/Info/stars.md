7169575b15ef6589c3e03156568f8273f5d91fcc
Marco Vinciguerra <mvincig11@gmail.com>
2024-06-07 10:05:27 +0200

    add stars

---
+++ docs/Info/stars.md
@@ -0,0 +1,3 @@
+## ⭐️ Star History
+
+[![Star History Chart](https://api.star-history.com/svg?repos=VinciGit00/Scrapegraph-ai&type=Date)](https://star-history.com/#VinciGit00/Scrapegraph-ai&Date)
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,14 +43,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ScrapeGraphAI/docusaurus-docs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ScrapeGraphAI/docusaurus-docs',
         },
         theme: {
           customCss: './src/css/custom.css',
